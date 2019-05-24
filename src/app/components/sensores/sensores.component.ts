30854d1d184972d0ab50bb31f7c12e998046bffb
willial1393 <willial1393@gmail.com>
2019-05-24 01:44:41 -0500

    Sensores full

--- src/app/components/sensores/sensores.component.ts
+++ src/app/components/sensores/sensores.component.ts
@@ -1,18 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Sensores} from '../../models/sensores';
+import {UIChart} from 'primeng/chart';
+import {MatlabService} from '../../services/matlab.service';
 
 @Component({
     selector: 'app-sensores',
     templateUrl: './sensores.component.html',
     styleUrls: ['./sensores.component.css']
 })
-export class SensoresComponent implements OnInit {
-    value = 10;
+export class SensoresComponent implements OnInit, OnDestroy {
+
+    @ViewChild('chartTemperatura') chartTemperatura: UIChart;
+    @ViewChild('chartHumendad') chartHumendad: UIChart;
+    @ViewChild('chartNivel') chartNivel: UIChart;
+    @ViewChild('chartPH') chartPH: UIChart;
+
     temperatura: any;
     humedad: any;
     nivel: any;
     ph: any;
 
-    constructor() {
+    sensores: Sensores = new Sensores();
+    threadVerificacion: any;
+
+    constructor(private matlabService: MatlabService) {
         this.temperatura = {
             labels: ['Grados celsius °C'],
             datasets: [
@@ -20,7 +31,7 @@ export class SensoresComponent implements OnInit {
                     label: 'Temperatura',
                     backgroundColor: '#f50004',
                     borderColor: '#f50004',
-                    data: [100]
+                    data: [0]
                 }
             ]
         };
@@ -31,7 +42,7 @@ export class SensoresComponent implements OnInit {
                     label: 'Humedad',
                     backgroundColor: '#86f5f2',
                     borderColor: '#86f5f2',
-                    data: [80]
+                    data: [0]
                 }
             ]
         };
@@ -42,17 +53,16 @@ export class SensoresComponent implements OnInit {
                     label: 'Nivel 1',
                     backgroundColor: '#42A5F5',
                     borderColor: '#1E88E5',
-                    data: [65]
+                    data: [0]
                 },
                 {
                     label: 'Nivel 2',
                     backgroundColor: '#42A5F5',
                     borderColor: '#1E88E5',
-                    data: [30]
+                    data: [0]
                 }
             ]
         };
-
         this.ph = {
             labels: ['pH'],
             datasets: [
@@ -60,13 +70,36 @@ export class SensoresComponent implements OnInit {
                     label: 'pH',
                     backgroundColor: '#f5dc00',
                     borderColor: '#f5dc00',
-                    data: [8]
+                    data: [0]
                 }
             ]
         };
     }
 
+    getSensores() {
+        this.matlabService.getSensores().subscribe(res => {
+            Object.assign(this.sensores, res);
+            this.temperatura.datasets[0].data = [this.sensores.temperatura];
+            this.humedad.datasets[0].data = [this.sensores.humedad];
+            this.nivel.datasets[0].data = [this.sensores.nivel1];
+            this.nivel.datasets[1].data = [this.sensores.nivel2];
+            this.ph.datasets[0].data = [this.sensores.ph];
+
+            this.chartTemperatura.refresh();
+            this.chartHumendad.refresh();
+            this.chartNivel.refresh();
+            this.chartPH.refresh();
+        });
+    }
+
     ngOnInit() {
+        this.threadVerificacion = setInterval(function() {
+            this.getSensores();
+        }.bind(this), 1000);
+    }
+
+    ngOnDestroy() {
+        clearInterval(this.threadVerificacion);
     }
 
 }
